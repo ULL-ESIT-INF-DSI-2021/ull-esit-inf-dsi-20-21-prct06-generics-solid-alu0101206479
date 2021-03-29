789890c6e1facbe36930ab60d6e6a3599c0ef7ee
ACOIDAN MESA HERNANDEZ <alu0101026479@ull.edu.es>
2021-03-29 13:15:02 +0100

    clase DragonBall funcionando

---
+++ src/ejercicio-1/dragonball.ts
@@ -0,0 +1,31 @@
+import {Figther} from "./figther";
+
+export class DragonBall extends Figther {
+  constructor(nombre: string, peso: number, altura: number, private ki: number, estadisticasBasicas: number[]) {
+    super(nombre, peso, altura, estadisticasBasicas);
+  }
+
+  /**
+   * ```typescript
+   * // Ejemplo de llamada
+   *  dc.getTipo();
+   * ```
+   * Función que retorna el ki del personaje de Dragon Ball
+   * @return El atributo ki
+   */
+  public getKi() {
+    return this.ki;
+  }
+
+  /**
+   * ```typescript
+   * // Ejemplo de llamada
+   *  dc.setTipo(78);
+   * ```
+   * Función para cambiar el ki del personaje de Dragon Ball
+   * @param ki El nuevo ki del personaje de Dragon Ball
+   */
+  public setTipo(ki: number) {
+    this.ki = ki;
+  }
+}
--- tests/ejercicio-1.spec.ts
+++ tests/ejercicio-1.spec.ts
@@ -4,12 +4,14 @@ import {Pokemon} from '../src/ejercicio-1/pokemon';
 import {Marvel} from '../src/ejercicio-1/marvel';
 import {DC} from '../src/ejercicio-1/dc';
 import {StarWars} from '../src/ejercicio-1/starwars';
+import {DragonBall} from '../src/ejercicio-1/dragonball';
 
 describe('Pruebas del Ejercicio 1 - El combate definitivo', () => {
   const Venusaur = new Pokemon("Venusaur", 100, 2.0, "Planta", [82, 83, 80, 80]);
   const Ironman = new Marvel("Iron Man", 110, 1.92, "Superheroe", [85, 80, 110, 150]);
   const Batman = new DC("Batman", 90, 1.90, "Superheroe", [90, 85, 120, 160]);
   const Yoda = new StarWars("Yoda", 30, 1, "Verde", [110, 70, 150, 130]);
+  const Goku = new DragonBall("Goku", 93, 1.85, 85, [105, 105, 140, 150]);
   describe('Clase Figther y clases hijas', () => {
     describe('Se pueden instanciar los luchadores', () => {
       it('expect(Venusaur).not.be.equal(null);', () => {
@@ -24,13 +26,16 @@ describe('Pruebas del Ejercicio 1 - El combate definitivo', () => {
       it('expect(Yoda).not.be.equal(null);', () => {
         expect(Yoda).not.be.equal(null);
       });
+      it('expect(Goku).not.be.equal(null);', () => {
+        expect(Goku).not.be.equal(null);
+      });
     });
 
     describe('Funcionan los getters y setters', () => {
       it('expect(Venusaur.getNombre()).to.be.equal("Venusaur");', () => {
         expect(Venusaur.getNombre()).to.be.equal("Venusaur");
       });
-      it('expect(Venusaur.getPeso()).to.be.equal(110);', () => {
+      it('expect(Ironman.getPeso()).to.be.equal(110);', () => {
         expect(Ironman.getPeso()).to.be.equal(110);
       });
       it('Batman.setPeso(50);', () => {
@@ -38,37 +43,34 @@ describe('Pruebas del Ejercicio 1 - El combate definitivo', () => {
         expect(Batman.getPeso()).to.be.equal(50);
         Batman.setPeso(90);
       });
-      it('expect(Venusaur.getAltura()).to.be.equal(2);', () => {
-        expect(Venusaur.getAltura()).to.be.equal(2);
+      it('expect(Goku.getAltura()).to.be.equal(1.85);', () => {
+        expect(Goku.getAltura()).to.be.equal(1.85);
       });
       it('expect(Venusaur.getTipo()).to.be.equal("Planta");', () => {
         expect(Venusaur.getTipo()).to.be.equal("Planta");
       });
-      it('Venusaur.setTipo("Fuego");', () => {
-        Venusaur.setTipo("Fuego");
-        expect(Venusaur.getTipo()).to.be.equal("Fuego");
-        Venusaur.setTipo("Planta");
+      it('Ironman.setTipo("Malvado");', () => {
+        Ironman.setTipo("Malvado");
+        expect(Ironman.getTipo()).to.be.equal("Malvado");
+        Ironman.setTipo("Superheroe");
       });
-      it('expect(expect(Venusaur.getEstadisticasBasicas().ataque).to.be.equal(82);', () => {
-        expect(Venusaur.getEstadisticasBasicas().ataque).to.be.equal(82);
+      it('expect(Batman.getEstadisticasBasicas().ataque).to.be.equal(90);', () => {
+        expect(Batman.getEstadisticasBasicas().ataque).to.be.equal(90);
       });
-      it('expect(expect(Venusaur.getEstadisticasBasicas().defensa).to.be.equal(83);', () => {
-        expect(Venusaur.getEstadisticasBasicas().defensa).to.be.equal(83);
+      it('expect(Goku.getEstadisticasBasicas().defensa).to.be.equal(105);', () => {
+        expect(Goku.getEstadisticasBasicas().defensa).to.be.equal(105);
       });
       it('expect(expect(Venusaur.getEstadisticasBasicas().velocidad).to.be.equal(80);', () => {
         expect(Venusaur.getEstadisticasBasicas().velocidad).to.be.equal(80);
       });
-      it('expect(expect(Venusaur.getEstadisticasBasicas().hp).to.be.equal(80);', () => {
-        expect(Venusaur.getEstadisticasBasicas().hp).to.be.equal(80);
+      it('expect(Ironman.getEstadisticasBasicas().hp).to.be.equal(150);', () => {
+        expect(Ironman.getEstadisticasBasicas().hp).to.be.equal(150);
       });
-    });
-
-    describe('Funcionan el setHP', () => {
-      it('expect(expect(Venusaur.getEstadisticasBasicas().hp).to.be.equal(15);', () => {
-        Venusaur.setHP(15);
-        expect(Venusaur.getEstadisticasBasicas().hp).to.be.equal(15);
+      it('Batman.setHP(15);', () => {
+        Batman.setHP(15);
+        expect(Batman.getEstadisticasBasicas().hp).to.be.equal(15);
+        Batman.setHP(170);
       });
-      Venusaur.setHP(80);
     });
   });
 });

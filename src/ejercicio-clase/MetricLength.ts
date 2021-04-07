375ba1f7ed27dc07da37e749ebb966d68ece4c06
ACOIDAN MESA HERNANDEZ <alu0101026479@ull.edu.es>
2021-04-07 10:09:16 +0100

    Clase MetricLength funcionando

---
+++ src/ejercicio-clase/MetricLength.ts
@@ -0,0 +1,34 @@
+/**
+ * Clase que representa una cantidad de longitud en el sistema métrico
+ */
+export class MetricLength {
+  /**
+   * Constructor de la clase MetricLength, se pasa como parametro la cantidad de longitud en centímetros
+   * @param cantidad Cantidad de longitud en centímetros
+   */
+  constructor(private cantidad: number) {}
+
+  /**
+   * Función que me devuelve la cantidad en centímetros
+   * @returns El atributo cantidad, porque ya está en centímetros
+   */
+  getLongitudEnCentimetros() {
+    return this.cantidad;
+  }
+
+  /**
+   * Función que me devuelve la cantidad en metros
+   * @returns El atributo cantidad/100, porque está seria la conversión de centímetros a metros
+   */
+  getLongitudEnMetros() {
+    return this.cantidad/100;
+  }
+
+  /**
+   * Función que me devuelve la cantidad en kilómetros
+   * @returns El atributo cantidad/100, porque está seria la conversión de centímetros a kilómetros
+   */
+  getLongitudEnKilometros() {
+    return this.cantidad/100000;
+  }
+}
--- tests/ejercicio-clase.spec.ts
+++ tests/ejercicio-clase.spec.ts
@@ -1,6 +1,7 @@
 import 'mocha';
 import {expect} from 'chai';
 import {ImperialLength} from '../src/ejercicio-clase/ImperialLength';
+import {MetricLength} from '../src/ejercicio-clase/MetricLength';
 
 describe('Pruebas del ejercicio de clase del 7 de abril', () => {
   const longitudImperial = new ImperialLength(70000);
@@ -17,6 +18,21 @@ describe('Pruebas del ejercicio de clase del 7 de abril', () => {
       });
       it('expect(Math.floor(longitudImperial.getLongitudEnMillas())).to.be.equal(1);', () => {
         expect(Math.floor(longitudImperial.getLongitudEnMillas())).to.be.equal(1);
+      });
+    });
+  });
+
+  const longitudMetrica = new MetricLength(700000);
+  describe('Clase MetricLength', () => {
+    describe('Funciona los gets en las distintas unidades del sistema metrico', () => {
+      it('expect(longitudMetrica.getLongitudEnCentimetros()).to.be.equal(700000);', () => {
+        expect(longitudMetrica.getLongitudEnCentimetros()).to.be.equal(700000);
+      });
+      it('expect(longitudMetrica.getLongitudEnMetros()).to.be.equal(7000);', () => {
+        expect(longitudMetrica.getLongitudEnMetros()).to.be.equal(7000);
+      });
+      it('expect(longitudMetrica.getLongitudEnKilometros()).to.be.equal(7);', () => {
+        expect(longitudMetrica.getLongitudEnKilometros()).to.be.equal(7);
       });
     });
   });
